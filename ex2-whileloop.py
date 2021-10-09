ea64836e2935cc7163dcf39b9232c3f5ab440c16
Bijay Shrestha <bijayshrestha@Bijays-Air.attlocal.net>
2021-10-08 20:34:52 -0500

    All files committed

---
+++ ex2-whileloop.py
@@ -0,0 +1,10 @@
+def double(n):
+    return 2 * n     #Return twice the given number
+#Call the function with the value of your choice and print its result
+print("To call this function using its name and assign the value")
+
+#x = double(3)
+#print(x)
+print("The program ran successfully")
+#----------------------------------------------------------------------------------
+print("first modification for 2nd commit")
